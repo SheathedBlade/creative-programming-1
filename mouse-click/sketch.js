4bdf3626589546280419527f8d4b851d7db1a813
Andrew Chuah <warlordpluto@gmail.com>
2022-10-24 12:47:20 -0400

    mouseclick exercise

--- mouse-click/sketch.js
+++ mouse-click/sketch.js
@@ -1,7 +1,33 @@
+let cursorColor = "rgb(255,255,255)";
+
 function setup() {
-  createCanvas(400, 400);
+  createCanvas(windowWidth, windowHeight);
+  noCursor();
 }
 
 function draw() {
-  background(220);
+  background(150, 140, 220);
+  fill(cursorColor);
+  noStroke();
+  square(mouseX, mouseY, 30);
+}
+
+function mousePressed() {
+  console.log("im down");
+  cursorColor = "rgb(56,123,231)";
+}
+
+function mouseReleased() {
+  console.log("im up");
+  cursorColor = "rgb(255,255,255)";
+}
+
+function mouseDragged() {
+  console.log("help me");
+  cursorColor = "rgb(126,12,123)";
+}
+
+function doubleClicked() {
+  console.log("double click");
+  cursorColor = "rgb(0,0,0)";
 }
