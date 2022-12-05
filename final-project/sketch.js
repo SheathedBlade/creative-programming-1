534c4fe0d8f28841aab0ea957910615ab6cdff4a
Andrew Chuah <warlordpluto@gmail.com>
2022-12-05 15:46:07 -0500

    init final project, dystopia

---
+++ final-project/sketch.js
@@ -0,0 +1,54 @@
+function preload() {}
+
+function setup() {
+  canvasSize = createVector(windowWidth, windowHeight);
+
+  createCanvas(canvasSize.x, canvasSize.y);
+  noSmooth();
+  strokeCap(SQUARE);
+  textFont("Trebuchet MS");
+  angleMode(DEGREES);
+  rectMode(CENTER);
+}
+
+function draw() {
+  background(10, 20, 30);
+}
+
+// Promoted as new chancellor for efforts in eradicating disease
+function intro() {}
+
+// Welcome to new job, tutorial (day 1)
+function week1() {}
+
+function night1() {}
+
+// Food supply chain
+function week2() {}
+
+function night2() {}
+
+// Insurgents rising
+function week3() {}
+
+function night3() {}
+
+// Caught insurgents, what to do?
+function week4() {}
+
+function night4() {}
+
+// Violence breaking out at main square, what to do?
+function week5() {}
+
+function night5() {}
+
+// Civil war, Peacekeepers are ready to deploy (either send in more soldiers, or deploy Peacekeepers)
+function week6() {}
+
+function night6() {}
+
+// One choice: fight
+function week7() {}
+
+function ending() {}
